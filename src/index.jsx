0e342c1e71d7dec767092b95792dd2870524f1b2
stefxmas <143958588+stefxmas@users.noreply.github.com>
2024-09-08 20:25:44 -0400

    still adding deleted code
    scss

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,9 @@ import { createRoot } from 'react-dom/client';
 import { MainView } from './components/main-view/main-view';
 
 // Import statement to indicate that you need to bundle `./index.scss`
+// import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.scss";
+import Container from 'react-bootstrap/Container';
 
 // Main component (will eventually use all the others)
 // const MyFlixApplication = () => {
@@ -14,7 +16,11 @@ import "./index.scss";
 //   );
 // };
 const App = () => {
-  return <MainView />;
+  return (
+    <Container>
+    <MainView />
+  </Container>
+  );
  };
 
 // Finds the root of your app
